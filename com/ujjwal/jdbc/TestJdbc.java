2a21ded33ad0a05df384d9f4615ed2b3d315da09
uk47kumar <uk47kumar@gmail.com>
2022-10-10 03:36:57 +0530

    entity

---
+++ com/ujjwal/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.ujjwal.jdbc;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		try {
+			System.out.println("Connecting to database " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+		}
+		
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
